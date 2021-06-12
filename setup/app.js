0f476086b42f82ac5ccb313ea0efa60d5746ef44
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-12 20:02:04 +0600

    Added tons of scripts.
    After a decade of working we've successfully added all the necessary scripts to make everything work properly. We even made a bug and solved it. Congratulations to us.

--- setup/app.js
+++ setup/app.js
@@ -71,4 +71,79 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "Steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/steak-dinner.jpg",
+    desc: `steak major paurse authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+// select items
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+// event to load items
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItem(menu);
+  displayMenuBtns();
+})
+
+// function to display menu items
+function displayMenuItem (menuItem) {
+  let displayMenu = menuItem.map(function (item) {
+    
+    return `<article class="menu-item">
+    <img src=${item.img} alt=${item.ti} class="photo" />
+    <div class="item-info">
+    <header>
+    <h4>${item.title}</h4>
+    <h4 class="price">${item.price}</h4>
+    </header>
+    <p class="">
+    ${item.desc}
+    </p>
+    </div>
+    </article>`;
+  })
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+}
+
+// function to display category buttons
+function displayMenuBtns() {
+  const categories = menu.reduce(function (values, item) {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },["all"]);
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-id=${category}>
+    ${category}
+    </button>`;
+  }).join("");
+  btnContainer.innerHTML = categoryBtns;
+  console.log(categoryBtns);
+  
+  // selecting buttons after displaying them properly on page
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  
+  // event to filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      })
+      if ( category === "all") {
+        displayMenuItem(menu);
+      } else {
+        displayMenuItem(menuCategory);
+      }
+    })
+  })
+}
